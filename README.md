8cff92aec3152e7cf491ed13f0026ee4d28ab19c
Sofia <ana-sofia.97@hotmail.com>
2020-11-03 20:53:55 +0000

    project  update

---
+++ README.md
@@ -0,0 +1,9 @@
+<h3> Projeto Calculadora - Project Calculator</h3>
+
+> I am trying to create some projects to practice and learn more about javascript.
+
+- HTML - Use of Bootsrap
+
+
+
+
---
+++ main.js
@@ -0,0 +1,24 @@
+function calculate(tipo, valor) {
+
+  if (tipo === 'acao') {
+    if (valor === 'c') {
+      document.querySelector('.input1').value = '';
+    };
+
+    if (valor === '/' || valor === '*' || valor === '-' || valor === '+' || valor === '.') {
+      document.querySelector('.input1').value += valor;
+    };
+
+
+    if (valor === '=') {
+      let resultInput = eval(document.querySelector('.input1').value);
+      document.querySelector('.input1').value = resultInput;
+    }
+
+  }
+  else if (tipo === 'valor') {
+    document.querySelector(".input1").value += valor;
+  }
+  console.log(valor)
+
+};
